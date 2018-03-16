91b5db60de3621ece7028684e76cbfe9c5aaf57c
Anthony Tucto <anthony.tucto.m@gmail.com>
2018-03-16 14:08:15 -0500

    Ticket: Controlador, Modelo, Vista

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+
+Route::get('/register','TicketsController@index');
+
+Route::get('/list','TicketsController@list');
